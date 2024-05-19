e7208cc8971d5c0ce1b6b1177e736801b33f62f0
Tofu <osawa.shuto@gmail.com>
2024-05-19 12:49:12 +0900

    removed files

--- Dir1X/file1x.py
+++
@@ -1,5 +0,0 @@
-def print1X():
-    print('this is dir1X file1x')
-
-def toBeImportedX():
-    print("I am called from outside")
--- Dir2X/file2x.py
+++
@@ -1,7 +0,0 @@
-
-from Dir1X.file1x import toBeImportedX
-def print2X():
-    print("this is dir2X file2x")
-
-def loadFromDir1X():
-    toBeImportedX()
